a695f6b23e4db773236e6cc790c33b4111e49ec8
mah-davarfa <mah.davarfa@gmail.com>
2025-03-17 14:28:08 -0400

    used grid it looks good to me more than flex

--- src/component/Playlist.jsx
+++ src/component/Playlist.jsx
@@ -28,46 +28,54 @@ const Playlist=()=>{
     );
     }
         
-    return(
-    <div className ='list'>
-      
-           <div className={'playlist-title'}>
-            {playlist.length>0 &&(
-               
-               <div className={'playlist-header'}>             
+    return (
+        <div className="playlist-container">
+          {playlist.length > 0 && (
+            <div className="playlist-title">
+              <div className="playlist-header">
                 {playlistLimitReached ? (
-                <h3 className="warning-text">
-                    'Playlist have reached to the limit of 10 songs,Either Transfer to spotify or Remove some songs'
-                </h3>
-                ):(
-                    <div className="playlist-header">
-                    {!isEditing? ( <h3>{playlistTitle}</h3>):(<input  onChange={handlePlaylistTitle} value={playlistTitle} type='text' placeholder='pick a name for  Title'></input>) }
-                        <button onClick={handleEdit}>{!isEditing ? "Edit" : "save"}</button>
-                        <button>Save to Spotify</button>
-                    </div>)}
-               </div>)}
-           </div>
-                
-        {playlist.map((song, index)=>(
-        <div key={index} className ='playlist-item-card'>
-            <img src={song.image} alt={song.name}  
-            width={150} height={150}/>
-            <h3> {song.name}</h3>
-            <p>Artist: {song.artist}</p>
-            <p>Album: {song.album}</p>
-            <p>popularity:{song.popularity}</p>
-           { /*<p>uri:{song.uri}</p>*/}
-           {/* <p>ID:{song.id}</p>*/}
-            <button onClick={()=>handleRemove(song.id,'playlist')}>Remove this song</button>
-            <button>Add to Spotify</button>
-            <button onClick={()=>handlePlay(song)}>
-                {currentSong===song.id ? 'Now Playing':'Play'}
-            </button>
-        </div>
+                  <h3 className="warning-text">
+                    Playlist has reached limit of 10 songs. Either Transfer or Remove.
+                  </h3>
+                ) : (
+                  <>
+                    {!isEditing ? (
+                      <h3>{playlistTitle}</h3>
+                    ) : (
+                      <input
+                        onChange={handlePlaylistTitle}
+                        value={playlistTitle}
+                        type="text"
+                        placeholder="Pick a name for title"
+                      />
+                    )}
+                    <button onClick={handleEdit}>{!isEditing ? 'Edit' : 'Save'}</button>
+                    <button>Save to Spotify</button>
+                  </>
+                )}
+              </div>
+            </div>
+          )}
       
-    ))}
-    
-    
-    </div>
-)};
+          <div className="playlist-list">
+            {playlist.map((song, index) => (
+              <div key={index} className="playlist-item-card">
+                <img src={song.image} alt={song.name} width={150} height={150} />
+                <h3>{song.name}</h3>
+                <p>Artist: {song.artist}</p>
+                <p>Album: {song.album}</p>
+                <p>popularity: {song.popularity}</p>
+                <button onClick={() => handleRemove(song.id, 'playlist')}>Remove this song</button>
+                <button>Add to Spotify</button>
+                <button onClick={() => handlePlay(song)}>
+                  {currentSong === song.id ? 'Now Playing' : 'Play'}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+      
+  
+};
 export default Playlist;
