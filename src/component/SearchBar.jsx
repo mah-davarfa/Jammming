fed8e2a632ac2229392f11f2ebc842a2bec96623
mah-davarfa <mah.davarfa@gmail.com>
2025-04-30 16:03:10 -0400

    Temporarily remove SearchBar.css import

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { AppContext } from "../context/AppContext.jsx";
-import "../styles/SearchBar.css";
+
 import "../styles/darkMode.css";
 
 export default function SearchBar() {
